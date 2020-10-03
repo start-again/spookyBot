3b13298b5d15971608a530a65c8565c6964072f0
Thomas Metten <t.metten@aca-it.be>
2020-10-03 18:11:11 +0200

    :rocket: Add dutch translation

---
+++ lang/nl.js
@@ -0,0 +1,38 @@
+module.exports = {
+  lang: 'nl',
+  words: [
+    {
+      // Please do not delete this one 🙏
+      name: 'spooky',
+      emoji: '761602615326146590',
+    },
+    {
+      name: 'snoep',
+      emoji: '🍬',
+    },
+    {
+      name: 'spook',
+      emoji: '👻',
+    },
+    {
+      name: 'halloween',
+      emoji: '🎃',
+    },
+    {
+      name: 'vampier',
+      emoji: '🧛🏻‍♂️',
+    },
+    {
+      name: 'tovenaar',
+      emoji: '🧙',
+    },
+    {
+      name: 'zombie',
+      emoji: '🧟',
+    },
+    {
+      name: 'ondode',
+      emoji: '🧟',
+    },
+  ],
+}
