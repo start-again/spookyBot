a3ffed04f249a57419383ed56f7f746b1b7e5ebd
Andrew <merrillman32@gmail.com>
2020-10-03 15:48:03 -0600

    removed console.log

--- utils/reactMessage.js
+++ utils/reactMessage.js
@@ -8,7 +8,6 @@ module.exports = async (guildId, message) => {
   const guildData = await getGuild(guildId)
   const translation = await require(`../lang/${guildData.lang}.js`)
   const fullMessage = message.content.toLowerCase()
-  console.log(fullMessage)
 
   translation.words.forEach((w) => {
     if (fullMessage.contains(w.name)) message.react(w.emoji)
