d2629ccf19088404382700d9a0280fabcd6c6365
Matteo A <augermtt@gmail.com>
2020-10-03 22:36:16 +0200

    feat(translation): add pumpkin ca translation

--- lang/ca.js
+++ lang/ca.js
@@ -1,23 +1,23 @@
 module.exports = {
-    lang: 'ca',
-    words: [
-      {
-        // Please do not delete this one 🙏
-        name: 'spooky',
-        emoji: '761602615326146590',
-      },
-      {
-        name: 'caramel',
-        emoji: '🍬',
-      },
-      {
-        name: 'fantasma',
-        emoji: '👻',
-      },
-      {
-        name: 'halloween',
-        emoji: '🎃',
-      },
-    ],
-  }
-  
+  lang: 'ca',
+  words: [
+    {
+      // Please do not delete this one 🙏
+      name: 'spooky',
+      emoji: '761602615326146590',
+    },
+    {
+      name: 'caramel',
+      emoji: '🍬',
+    },
+    {
+      name: 'fantasma',
+      emoji: '👻',
+    },
+    {
+      name: ['halloween', 'pumpkin'],
+      emoji: '🎃',
+    },
+  ],
+}
+
