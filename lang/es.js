bbdddb7075d7a97730c954077b9c84c11cdcf2f8
amerrill <merrillman32@gmail.com>
2020-10-03 07:54:04 -0600

    added spanish language file

---
+++ lang/es.js
@@ -0,0 +1,34 @@
+module.exports = {
+  lang: 'es',
+  words: [
+    {
+      // Please do not delete this one 🙏
+      name: 'spooky',
+      emoji: '761602615326146590',
+    },
+    {
+      name: 'dulce',
+      emoji: '🍬',
+    },
+    {
+      name: 'caramelo',
+      emoji: '🍬',
+    },
+    {
+      name: 'fantasma',
+      emoji: '👻',
+    },
+    {
+      name: 'halloween',
+      emoji: '🎃',
+    },
+    {
+      name: 'noche de brujas',
+      emoji: '🎃',
+    },
+    {
+      name: 'dia de brujas',
+      emoji: '🎃',
+    },
+  ],
+}
