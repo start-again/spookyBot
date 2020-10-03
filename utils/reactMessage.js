178f71d040d3fe4f01b50c6a25791323b5da7599
Andrew <merrillman32@gmail.com>
2020-10-03 14:42:14 -0600

    fixed formatting

--- utils/reactMessage.js
+++ utils/reactMessage.js
@@ -4,7 +4,7 @@ module.exports = async (guildId, message) => {
   const guildData = await getGuild(guildId)
   const translation = await require(`../lang/${guildData.lang}.js`)
   const words = message.content.toLowerCase().trim().split(/ +/g)
-  
+ 
   words.forEach((word) => {
     const wordData = translation.words.find((w) => w.name == word)
     if (wordData != undefined) message.react(wordData.emoji)
