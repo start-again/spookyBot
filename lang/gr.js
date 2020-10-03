f37b36cbd99d2615d12c1bb5ca487b3e02561c1f
Pervolarakis <56447146+Pervolarakis@users.noreply.github.com>
2020-10-03 21:29:31 +0300

    Create gr.js
    added greek language

---
+++ lang/gr.js
@@ -0,0 +1,38 @@
+module.exports = {
+  lang: 'en',
+  words: [
+    {
+      // Please do not delete this one 🙏
+      name: 'spooky',
+      emoji: '761602615326146590',
+    },
+    {
+      name: 'Καραμέλα',
+      emoji: '🍬',
+    },
+    {
+      name: 'Φάντασμα',
+      emoji: '👻',
+    },
+    {
+      name: 'Απόκριες',
+      emoji: '🎃',
+    },
+    {
+      name: 'Βρυκόλακας',
+      emoji: '🧛🏻‍♂️',
+    },
+    {
+      name: 'Μάγισσα',
+      emoji: '🧙',
+    },
+    {
+      name: 'Ζόμπι',
+      emoji: '🧟',
+    },
+    {
+      name: 'Ιστός',
+      emoji: '🕸',
+    }
+  ],
+}
