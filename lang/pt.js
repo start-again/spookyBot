8cd9a2a15c96ba2f2ba4101afbaa6773eb4792e5
Meika Oliveira <meikafoliveira@gmail.com>
2020-10-04 17:56:40 -0300

    Added Portuguese translation

---
+++ lang/pt.js
@@ -0,0 +1,39 @@
+module.exports = {
+    lang: 'pt',
+    words: [
+      {
+        // Please do not delete this one 🙏
+        name: 'spooky',
+        emoji: '761602615326146590',
+      },
+      {
+        name: ['doce', 'doces'],
+        emoji: '🍬',
+      },
+      {
+        name: ['fantasma', 'alma'],
+        emoji: '👻',
+      },
+      {
+        name: ['halloween', 'abóbora', 'dia das bruxas'],
+        emoji: '🎃',
+      },
+      {
+        name: ['vampiro', 'drácula'],
+        emoji: '🧛🏻‍♂️',
+      },
+      {
+        name: ['bruxo', 'mago', 'feiticeiro'],
+        emoji: '🧙',
+      },
+      {
+        name: 'zumbi',
+        emoji: '🧟',
+      },
+      {
+        name: ['teia', 'teia de aranha'],
+        emoji: '🕸',
+      },
+    ],
+  }
+  
