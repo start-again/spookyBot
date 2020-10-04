5be2100fc343502452a050aa62f4145ec99c292d
Thomas Metten <t.metten@aca-it.be>
2020-10-04 20:32:15 +0200

    :lips: Add story command

---
+++ commands/Text/story.js
@@ -0,0 +1,35 @@
+const snoowrap = require('snoowrap')
+const { MessageEmbed } = require('discord.js')
+const { prefix, reddit } = require('../../config.json')
+
+module.exports = {
+  config: {
+    name: 'Scary story',
+    usage: `${prefix}story`,
+    description: 'Tell a scary story (from reddit.com/r/scarystories)',
+    command: 'story',
+    aliases: ['ghoststory', 'tale'],
+    displayHelp: false,
+  },
+
+  run: async (bot, message, args) => {
+    const retriever = new snoowrap({
+      userAgent: reddit.clientId,
+      clientId: reddit.clientId,
+      clientSecret: reddit.clientSecret,
+      refreshToken: reddit.refreshToken,
+    })
+
+    const stories = await retriever.getSubreddit('scarystories').getHot()
+    const story = stories[[Math.floor(Math.random() * stories.length)]]
+
+    story.selftext.match(/(.|[\r\n]){1,2048}/g).forEach((storyPart, partIndex, parts) => {
+      let embed = new MessageEmbed()
+        .setColor('881EE4')
+        .setTitle(`${story.title} (${partIndex + 1}/${parts.length})`)
+        .setDescription(storyPart)
+        .setFooter(`Author: ${story.author.name}`)
+      message.channel.send(embed)
+    })
+  },
+}
