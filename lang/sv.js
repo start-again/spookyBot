6a283bfb71087535857439760718e644cb884c61
Hannes Fant <hqnnes@gmail.com>
2020-10-06 18:02:00 +0300

    Added swedish translation

---
+++ lang/sv.js
@@ -0,0 +1,38 @@
+module.exports = {
+  lang: 'sv',
+  words: [
+    {
+      // Please do not delete this one 🙏
+      name: 'spooky',
+      emoji: '761602615326146590',
+    },
+    {
+      name: ['godis'],
+      emoji: '🍬',
+    },
+    {
+      name: 'spöke',
+      emoji: '👻',
+    },
+    {
+      name: ['halloween', 'pumpa', 'allhelgonadagen', 'alla helgons dag'],
+      emoji: '🎃',
+    },
+    {
+      name: 'vampyr',
+      emoji: '🧛🏻‍♂️',
+    },
+    {
+      name: 'trollkarl',
+      emoji: '🧙',
+    },
+    {
+      name: 'zombie',
+      emoji: '🧟',
+    },
+    {
+      name: 'spindelnät',
+      emoji: '🕸',
+    },
+  ],
+}
