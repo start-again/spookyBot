748fdfcddfcf267af8160cd287af30cb77f6e709
gituser <gituser@synds.nadaniente>
2020-10-07 22:21:13 +0200

    Removed uniqueNames and translated headstone.

--- lang/de.js
+++ lang/de.js
@@ -8,400 +8,321 @@ module.exports = {
     },
    {
       name: ['alien', 'außerirdische', 'außerirdischer'],
-      uniqueName: 'Alien',
       emoji: '👽',
     },
     {
       name: ['alienmonster', 'monster'],
-      uniqueName: 'Alien Monster',
       emoji: '👾',
     },
 // Coming with Unicode/Emoji 13.0 this year, e.g. with Android 11
 //    {
 //      name: ['herz', 'anatomisches herz'],
-//      uniqueName: 'Anatomical Heart',
 //      emoji: '🫀',
 //    },
     {
       name: ['wütender teufel'],
-      uniqueName: 'Angry Face with Horns',
       emoji: '👿',
     },
     {
       name: ['unruhig', 'besorgt'],
-      uniqueName: 'Anxious Face with Sweat',
       emoji: '😰',
     },
     {
       name: ['axt'],
-      uniqueName: 'Axe',
       emoji: '🪓',
     },
     {
       name: ['fledermaus'],
-      uniqueName: 'Bat',
       emoji: '🦇',
     },
     {
       name: ['schlagendes herz'],
-      uniqueName: 'Beating Heart',
       emoji: '💓',
     },
     {
       name: ['schwarzes herz'],
-      uniqueName: 'Black Heart',
       emoji: '🖤',
     },
     {
       name: ['schwarze katze'],
-      uniqueName: 'Black Cat',
       emoji: '🐈‍⬛',
     },
     {
       name: ['blut'],
-      uniqueName: 'Blood',
       emoji: '🩸',
     },
     {
       name: ['blutstropfen'],
-      uniqueName: 'Drop of Blood',
       emoji: '🩸',
     },
     {
       name: ['knochen'],
-      uniqueName: 'Bone',
       emoji: '🦴',
     },
     {
       name: ['gehirn', 'brain'],
-      uniqueName: 'Brain',
       emoji: '🧠',
     },
     {
       name: ['besen'],
-      uniqueName: 'Broom',
       emoji: '🧹',
     },
     {
       name: ['kerze'],
-      uniqueName: 'Candle',
       emoji: '🕯',
     },
     {
       name: ['candy', 'bonbon', 'süßigkeit'],
-      uniqueName: 'Candy',
       emoji: '🍬',
     },
     {
       name: ['ketten'],
-      uniqueName: 'Chains',
       emoji: '⛓',
     },
     {
       name: ['schokoladenriegel', 'schokoriegel'],
-      uniqueName: 'Chocolate Bar',
       emoji: '🍫',
     },
     {
       name: ['blitz', 'gewitterwolke'],
-      uniqueName: 'Cloud with Lightning',
       emoji: '🌩',
     },
     {
       name: ['clown'],
-      uniqueName: 'Clown Face',
       emoji: '🤡',
     },
     {
       name: ['sarg'],
-      uniqueName: 'Coffin',
       emoji: '⚰',
     },
     {
       name: ['kristallkugel', 'hellseherkugel'],
-      uniqueName: 'Crystal Ball',
       emoji: '🔮',
     },
     {
       name: ['messer'],
-      uniqueName: 'Dagger',
       emoji: '🗡',
     },
 // Coming with Unicode/Emoji 13.0 this year, e.g. with Android 11
 //    {
 //      name: ['verkleidetes gesicht', 'verkleidet'],
-//      uniqueName: 'Disguised Face',
 //      emoji: '🥸',
 //    },
     {
       name: ['verlassenes haus', 'geisterhaus', 'geistervilla'],
-      uniqueName: 'Derelict House',
       emoji: '🏚',
     },
     {
       name: ['ohr'],
-      uniqueName: 'Ear',
       emoji: '👂',
     },
     {
       name: ['auge'],
-      uniqueName: 'Eye',
       emoji: '👁️',
     },
     {
       name: ['augen'],
-      uniqueName: 'Eyes',
       emoji: '👀',
     },
     {
       name: ['untersuchen'],
-      uniqueName: 'Detective',
       emoji: '🕵',
     },
     {
       name: ['elf'],
-      uniqueName: 'Elf',
       emoji: '🧝',
     },
     {
       name: ['schrei', 'schreie', 'schreien'],
-      uniqueName: 'Face Screaming in Fear',
       emoji: '😱',
     },
     {
       name: ['mumie'],
-      uniqueName: 'Face with Head-Bandage',
       emoji: '🤕',
     },
     {
       name: ['fee'],
-      uniqueName: 'Fairy',
       emoji: '🧚',
     },
     {
       name: ['angst', 'ängstlich'],
-      uniqueName: 'Fearful Face',
       emoji: '😨',
     },
     {
       name: ['feuer'],
-      uniqueName: 'Fire',
       emoji: '🔥',
     },
     {
       name: ['ufo', 'fliegende untertasse'],
-      uniqueName: 'Flying Saucer',
       emoji: '🛸',
     },
     {
       name: ['nebel', 'nebelig', 'neblig'],
-      uniqueName: 'Fog',
       emoji: '🌫️',
     },
     {
       name: ['fuß'],
-      uniqueName: 'Foot',
       emoji: '🦶',
     },
     {
       name: ['fußspuren'],
-      uniqueName: 'Footprints',
       emoji: '👣',
     },
     {
       name: ['vollmond'],
-      uniqueName: 'Full Moon',
       emoji: '🌕',
     },
     {
       name: ['urne'],
-      uniqueName: 'Funeral Urn',
       emoji: '⚱',
     },
     {
       name: ['genie'],
-      uniqueName: 'Genie',
       emoji: '🧞',
     },
     {
       name: ['goblin'],
-      uniqueName: 'Goblin',
       emoji: '👺',
     },
     {
       name: ['geist'],
-      uniqueName: 'Ghost',
       emoji: '👻',
     },
     {
       name: ['grünes herz'],
-      uniqueName: 'Green Heart',
       emoji: '💚',
     },
 // Coming with Unicode/Emoji 13.0 this year, e.g. with Android 11
 //    {
-//      name: ['headstone'],
-//      uniqueName: 'Headstone',
+//      name: ['grabstein', 'grab'],
 //      emoji: '🪦',
 //    },
     {
       name: ['hochspannung', 'spannend'],
-      uniqueName: 'High Voltage',
       emoji: '⚡',
     },
     {
       name: ['halloween', 'kürbis', 'hellowien'],
-      uniqueName: 'Jack-O-Lantern',
       emoji: '🎃',
     },
     {
       name: ['bein'],
-      uniqueName: 'Leg',
       emoji: '🦵',
     },
     {
       name: ['lollipop', 'lutscher'],
-      uniqueName: 'Lollipop',
       emoji: '🍭',
     },
     {
       name: ['zauberhaft', 'hexer', 'magier', 'zauberer', 'zaubrer'],
-      uniqueName: 'Mage',
       emoji: '🧙',
     },
     {
       name: ['meerjungfrau'],
-      uniqueName: 'Mermaid',
       emoji: '🧜‍♀️',
     },
     {
       name: ['meermann'],
-      uniqueName: 'Merman',
       emoji: '🧜‍♂️',
     },
     {
       name: ['meermensch'],
-      uniqueName: 'Merperson',
       emoji: '🧜',
     },
     {
       name: ['neumond', 'neumondgesicht'],
-      uniqueName: 'New Moon Face',
       emoji: '🌚',
     },
     {
       name: ['sternennacht', 'nacht', 'sternenklare nacht'],
-      uniqueName: 'Night with Stars',
       emoji: '🌃',
     },
     {
       name: ['ogre', 'oger'],
-      uniqueName: 'Ogre',
       emoji: '👹',
     },
     {
       name: ['oranges herz', 'orangenes herz'],
-      uniqueName: 'Orange Heart',
       emoji: '🧡',
     },
     {
       name: ['eule'],
-      uniqueName: 'Owl',
       emoji: '🦉',
     },
     {
       name: ['verkleidung', 'verkleidungen', 'verkleiden', 'verkleide' ],
-      uniqueName: 'Performing Arts',
       emoji: '🎭',
     },
     {
       name: ['schwebende person im anzug', 'schweben', 'schwebe', 'schwebt'],
-      uniqueName: 'Person In Suit Levitating',
       emoji: '🕴',
     },
     {
       name: ['prinzessin'],
-      uniqueName: 'Princess',
       emoji: '👸',
     },
     {
       name: ['roboter', 'maschinenmann', 'bot'],
-      uniqueName: 'Robot',
       emoji: '🤖',
     },
     {
       name: ['schädel', 'totenschädel', 'totenkopf'],
-      uniqueName: 'Skull',
       emoji: '💀',
     },
     {
       name: ['totenkopf mit knochen', 'totenkopfflagge', 'pirat'],
-      uniqueName: 'Skull and Crossbones',
       emoji: '☠',
     },
     {
       name: ['lächelnder teufel', 'teufel', 'teuflisch'],
-      uniqueName: 'Smiling Face with Horns',
       emoji: '😈',
     },
     {
       name: ['spinne'],
-      uniqueName: 'Spider',
       emoji: '🕷',
     },
     {
       name: ['netz', 'spinnennetz'],
-      uniqueName: 'Spider Web',
       emoji: '🕸',
     },
     {
       name: ['zunge'],
-      uniqueName: 'Tongue',
       emoji: '👅',
     },
     {
       name: ['einhorn'],
-      uniqueName: 'Unicorn',
       emoji: '🦄',
     },
     {
       name: ['vampir'],
-      uniqueName: 'Vampire',
       emoji: '🧛',
     },
     {
       name: ['blume', 'blüte', 'verwelkt'],
-      uniqueName: 'Wilted Flower',
       emoji: '🥀',
     },
     {
       name: ['elfin'],
-      uniqueName: 'Woman Elf',
       emoji: '🧝‍♀️',
     },
     {
       name: ['weibliche genie'],
-      uniqueName: 'Woman Genie',
       emoji: '🧞‍♀️',
     },
     {
       name: ['hexe', 'magierin', 'zauberin', 'zaubrerin'],
-      uniqueName: 'Woman Mage',
       emoji: '🧙‍♀️',
     },
     {
       name: ['vampirin'],
-      uniqueName: 'Woman Vampire',
       emoji: '🧛‍♀️',
     },
     {
       name: ['weiblicher zombie'],
-      uniqueName: 'Woman Zombie',
       emoji: '🧟‍♀️',
     },
     {
       name: ['zombie', 'untote'],
-      uniqueName: 'Zombie',
       emoji: '🧟',
     }
   ],
