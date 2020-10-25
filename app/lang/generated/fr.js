7ee54ff6078167c14a1da6c1c05d252cdad36a00
gituser <gituser@synds.nadaniente>
2020-10-25 15:19:01 +0100

    Updated the spreadsheet file with LibreOffice by changing the structure of on how to add words, enhanced the macro, and made the HowTo description including updated README.md file.

---
+++ app/lang/generated/fr.js
@@ -0,0 +1,142 @@
+module.exports = {
+  lang: 'fr', // French
+  words: [
+    {
+      // Please do not delete this one 🙏
+      name: ['spooky'],
+      emoji: '761602615326146590',
+    },
+    {
+      name: ['monstre'],
+      emoji: '👾',
+    },
+    {
+      name: ['alien', 'extraterrestre'],
+      emoji: '👽',
+    },
+    {
+      name: ['anxieux'],
+      emoji: '😰',
+    },
+    {
+      name: ['chauve-souris', 'chauve souris'],
+      emoji: '🦇',
+    },
+    {
+      name: ['os'],
+      emoji: '🦴',
+    },
+    {
+      name: ['cerveau', 'brain'],
+      emoji: '🧠',
+    },
+    {
+      name: ['balais'],
+      emoji: '🧹',
+    },
+    {
+      name: ['cierge', 'bougie'],
+      emoji: '🕯',
+    },
+    {
+      name: ['bonbon', 'friandise'],
+      emoji: '🍬',
+    },
+    {
+      name: ['chaines'],
+      emoji: '⛓',
+    },
+    {
+      name: ['clown'],
+      emoji: '🤡',
+    },
+    {
+      name: ['cerceuil'],
+      emoji: '⚰',
+    },
+    {
+      name: ['boule de crystal'],
+      emoji: '🔮',
+    },
+    {
+      name: ['dague', 'couteau'],
+      emoji: '🗡',
+    },
+    {
+      name: ['maison abandonnée'],
+      emoji: '🏚',
+    },
+    {
+      name: ['cri'],
+      emoji: '😱',
+    },
+    {
+      name: ['peur'],
+      emoji: '😨',
+    },
+    {
+      name: ['feu', 'fire'],
+      emoji: '🔥',
+    },
+    {
+      name: ['pleine lune'],
+      emoji: '🌕',
+    },
+    {
+      name: ['urne'],
+      emoji: '⚱',
+    },
+    {
+      name: ['fantôme', 'fantome'],
+      emoji: '👻',
+    },
+    {
+      name: ['halloween', 'citrouille', 'jack-o-lantern'],
+      emoji: '🎃',
+    },
+    {
+      name: ['sucette'],
+      emoji: '🍭',
+    },
+    {
+      name: ['sorcier'],
+      emoji: '🧙',
+    },
+    {
+      name: ['hibou'],
+      emoji: '🦉',
+    },
+    {
+      name: ['crâne', 'crane'],
+      emoji: '💀',
+    },
+    {
+      name: ['diable', 'satan'],
+      emoji: '😈',
+    },
+    {
+      name: ['toile'],
+      emoji: '🕸',
+    },
+    {
+      name: ['araignée', 'araignee'],
+      emoji: '🕷',
+    },
+    {
+      name: ['vampire'],
+      emoji: '🧛',
+    },
+    {
+      name: ['fleur fané', 'fleur fané'],
+      emoji: '🥀',
+    },
+    {
+      name: ['sorcière', 'sorciere'],
+      emoji: '🧙‍♀️',
+    },
+    {
+      name: ['zombie', 'mort vivant'],
+      emoji: '🧟',
+    }
+  ]
+}
---
+++ app/lang/generated/hi.js
@@ -0,0 +1,34 @@
+module.exports = {
+  lang: 'hi', // Hindi
+  words: [
+    {
+      // Please do not delete this one 🙏
+      name: ['spooky'],
+      emoji: '761602615326146590',
+    },
+    {
+      name: ['toffee'],
+      emoji: '🍬',
+    },
+    {
+      name: ['bhoot'],
+      emoji: '👻',
+    },
+    {
+      name: ['darr'],
+      emoji: '🎃',
+    },
+    {
+      name: ['aatma'],
+      emoji: '💀',
+    },
+    {
+      name: ['saanp'],
+      emoji: '🐍',
+    },
+    {
+      name: ['makdi'],
+      emoji: '🕷',
+    },
+  ]
+}
